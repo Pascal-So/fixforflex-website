c9670b4b59d120934089cfa7c0f733377fd6b81c
Pascal Sommer <p@pascalsommer.ch>
2016-07-20 18:49:34 +0200

    finished header for mobile and desktop

--- v2/index.js
+++ v2/index.js
@@ -1,34 +1,148 @@
+var nav;
+var win;
+var bg;
+var fixed = false;
+var navpos;
+
 $(function(){
+	bg = $("#mainbg");
+	nav = $("nav");	
+
+	win = $(window);
+
+	var desktop = true;
+
+	
+	navpos = nav.offset().top;
+
+	win.resize(function(){
+
+		if(!fixed) navpos = nav.offset().top;
+		checkWidth();
+
+	});
+
+	function checkWidth(){
+
+		var limit = 700;
+
+		var width = win.width();
+
+
+
+		if(desktop && width<limit){
+			desktop = false;
+
+			unsetDesktop();
+			setupMobile();
+
+		}else if (!desktop && width>limit){
+			desktop = true;
+
+			unsetMobile();
+			setupDesktop();
+
+		}
+	}
+
+	setupDesktop();
+	checkWidth();
+
+
+});
+
+
+function setupMobile(){
+
+	bg.css("background-position", "-70px center");
+
+	console.log("switch to mobile view");
+
+	/*
+	<!--nav class="brightOverlay abs bottom rigth left flex fh fcenter uppercase crop z10 centertext "   DESKTOP DEFAULT, 700+ px screenwidth -->
+	<nav class="brightOverlay fix top right navContracted uppercase crop z10 centertext">
+	*/
+
+	nav.removeClass("abs bottom left flex fh fcenter");
+	nav.addClass("fix top navContracted");
+	$("#navtoggle").removeClass("hidden");
+
+
+	var extended = false;
+	nav.removeClass("navExtended");
+	nav.addClass("navContracted");
+
+
+	$("#navtoggle").click(function(){
+		toggleNav();
+	});
+
+	function toggleNav(){
+		if(!extended){
+			nav.removeClass("navContracted");
+			nav.addClass("navExtended");
+			extended = true;
+		}else{
+			nav.removeClass("navExtended");
+			nav.addClass("navContracted");
+			extended=false;
+		}
+	}
+}
+
+function unsetMobile(){
+
+	nav.removeClass("fix top navContracted navExtended");
+	nav.addClass("abs bottom left flex fh fcenter");	
+	$("#navtoggle").addClass("hidden");
+
+	nav.unbind();
+}
+
+function unsetDesktop(){
+	win.unbind("scroll");
+}
+
+
+function setupDesktop(){
+
+	console.log("switch to desktop view");
 
 	var pos, lastpos;
-	var bg = $("#mainbg");
-	var nav = $("nav");
-	var navpos = nav.offset().top;
 
-	$(window).scroll(function(){
-		pos = $(window).scrollTop();
+	
+
+	win.scroll(function(){
+		pos = win.scrollTop();
 		bgParallax(bg, pos);
 		navFix(nav, navpos, pos)
 	
 	});
 
-});
+	fixed = false;
+	
+	nav.addClass("abs bottom");
+	nav.removeClass("fix top");
 
-var fixed = false;
+	navFix(nav, navpos, win.scrollTop());
 
-function navFix(nav, navpos, pos){
-	if(fixed && pos < navpos){
-		nav.addClass("abs bottom");
-		nav.removeClass("fix top");
-		fixed = false;
-	}else if(!fixed && pos > navpos){
-		nav.removeClass("abs bottom");
-		nav.addClass("fix top");
-		fixed = true;
+	function navFix(nav, navpos, pos){
+		if(fixed && pos < navpos){
+			nav.addClass("abs bottom");
+			nav.removeClass("fix top");
+			fixed = false;
+		}else if(!fixed && pos > navpos){
+			nav.removeClass("abs bottom");
+			nav.addClass("fix top");
+			fixed = true;
+		}
 	}
+
+	function bgParallax(bg, pos){
+		var offset = pos/2;
+		bg.css("background-position", "center " + offset + "px");
+	}
+
+
 }
 
-function bgParallax(bg, pos){
-	var offset = pos/2;
-	bg.css("background-position", "center " + offset + "px");
-}
